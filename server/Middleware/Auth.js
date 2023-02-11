37e5f52d5e47911bd74bfc13c005b3ed2474011a
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-02-11 21:18:24 +0000

    handle token verification

--- server/Middleware/Auth.js
+++ server/Middleware/Auth.js
@@ -6,6 +6,14 @@ export const verifyToken = (req,  res, next) => {
 
     if(!token) return res.status(403).send("Access denied")
 
+    if(token.startsWith('Bearer ')){
+      token = token.slice(7, token.length).trimLeft()
+    }
+
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+    next();
+
   } catch (err){
     res.status(500).json({ error: err.message })
   }
