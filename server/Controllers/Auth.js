8c7891d012d40158770f3924b9784a03c87ee095
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-02-11 21:07:51 +0000

    check pwd match, provide token & remove password before sending data back to front end

--- server/Controllers/Auth.js
+++ server/Controllers/Auth.js
@@ -50,6 +50,14 @@ export const login = async (req, res) => {
 
     const user = await User.findOne({ email: email });
     if(!user) return res.status(400).json({ msg: 'User not found' })
+    
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return es.status(400).json({ msg: 'Invalid credentials' });
+
+    const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET);
+    delete user.password;
+
+    res.status(200).json({ token, user })
 
   } catch (err){
     res.status(500).json({ error: err.message })
