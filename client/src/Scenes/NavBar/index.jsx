ba6364cf2b1a2626ac45e49bf59088cd03682b0d
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-02-12 20:48:27 +0000

    ughhh

--- client/src/Scenes/NavBar/index.jsx
+++ client/src/Scenes/NavBar/index.jsx
@@ -1,6 +1,47 @@
-import React from 'react'
+
+import { useState } from "react";
+import {
+  useMediaQuery,
+  IconButton,
+  FormControl,
+  Typography,
+  InputBase,
+  useTheme,
+  MenuItem,
+  Select,
+  Box
+} from "@mui/material";
+import {
+  Notifications,
+  LightMode,
+  DarkMode,
+  Message,
+  Search,
+  Close,
+  Help,
+  Menu
+} from "@mui/icons-material";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "state";
+import { Navigate, useNavigate } from "react-router-dom";
+import FlexBetween from "Components/FlexBetween";
 
 const NavBar = () => {
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+  const isNonMobileScreens = useMediaQuery("min-width: 1000px")
+
+  const theme = useTheme();
+  const neutralLigt = theme.palette.neutral.light
+  const dark = theme.palette.neutral.dark
+  const background = theme.palette.background.default
+  const primaryLight = theme.palette.neutral.primary.light
+  const alt = theme.palette.neutral.alt;
+
+  const fullName = `${user.firstName} ${user.lastName};`
+
   return (
     <div>
       NavBar
