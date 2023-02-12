f0294b0e537d82bcf976468ee930bebddd64d7e4
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-02-12 12:14:35 +0000

    getFriends controller

--- server/Controllers/Users.js
+++ server/Controllers/Users.js
@@ -10,3 +10,24 @@ export const getuser = async (req, res) => {
     res.status(404).json({ message: err.message});
   }
 };
+
+export const getUserFriends = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+
+    const friends = await Promise.all(
+      user.friends.map((id) => User.findById(id))
+    );
+
+    const formattedFriends = friends.map(
+      ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+        return { _id, firstName, lastName, occupation, location, picturePath }
+      }
+    );
+    res.status(200).json(formattedFriends);
+
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
