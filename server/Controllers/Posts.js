15662fcb3c6cf3c2a02e84956c65d2077a3025a4
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-02-12 14:40:06 +0000

    feed posts and users posts

--- server/Controllers/Posts.js
+++ server/Controllers/Posts.js
@@ -28,3 +28,23 @@ export const createPost = async (req, res) => {
     res.status(409).json({ message: err.message})
   }
 };
+
+// Read
+export const getFeedPosts = async (req, res) => {
+  try{
+    const post = await Post.find();
+    res.status(200).json(post)
+  } catch (err) {
+    res.status(404).json({ message: err.message })
+  }
+};
+
+export const getUserPosts = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const post = await Post.find({ userId });
+    res.status(200).json(post)
+  } catch (err) {
+    res.status(404).json({ message: err.message })
+  }
+};
