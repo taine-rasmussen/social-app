5dc43a2cdd71c899c5115b49730aa1d42ef17814
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-02-12 12:47:18 +0000

    fix overwrite bug

--- server/Controllers/Auth.js
+++ server/Controllers/Auth.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import User from '../models/User.js'
+import User from '../Models/User.js'
 
 // Register User
 export const register = async (req, res) => {
