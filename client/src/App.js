3bce59156eb8e2cd2f25e79fbcceb9954b461c19
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-02-12 18:54:55 +0000

    react router routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import HomePage from 'scences/HomePage';
+import LoginPage from 'scences/LoginPage';
+import ProfilePage from 'scences/ProfilePage';
+
 
 function App() {
+
   return (
     <div className="App">
-        App
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LoginPage />}/>
+        <Route path='/home' element={<HomePage />} />
+        <Route path='/profile/:userId' element={<ProfilePage />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
