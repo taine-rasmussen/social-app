7dc9169c51082bc0e1af5b465f4285079a98a236
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-02-12 14:30:07 +0000

    PostsSchema

---
+++ server/Controllers/Posts.js
@@ -0,0 +1,1 @@
+import Post from '../Models/Posts.js'
---
+++ server/Models/Posts.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const PostSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true
+    },
+    firstname: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+    loaction: String,
+    description: String,
+    picturePath: String,
+    userPicturePath: String,
+    // Map over Array - O(1) v o(n)
+    likes: {
+      type: Map,
+      of: Boolean,
+    },
+    comments: {
+      type: Array,
+      default: []
+    }
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model('Post', PostSchema);
+export default Post
