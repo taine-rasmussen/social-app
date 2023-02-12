e8fa38e0ca619737372b29473acefabb5e1a90ff
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-02-12 14:37:06 +0000

    create new post

--- server/Controllers/Posts.js
+++ server/Controllers/Posts.js
@@ -1,1 +1,30 @@
 import Post from '../Models/Posts.js'
+import User from '../Models/User.js';
+
+// Create
+export const createPost = async (req, res) => {
+  try{
+    const { userId, description, picturePath } = req.body;
+    const user = await User.findById(userId);
+
+    const newPost = new Post({
+      userId,
+      firstname: user.firstName,
+      lastname: user.lastName,
+      location: user.location,
+      description,
+      userPicturePath: user.picturePath,
+      picturePath,
+      likes: {},
+      comments: []
+    });
+
+    await newPost.save();
+    const post = await Post.find();
+    res.status(201).json(post)
+
+    // Save new post and return all posts including new post to display
+  } catch (err){
+    res.status(409).json({ message: err.message})
+  }
+};
