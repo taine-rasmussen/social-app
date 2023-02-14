7fab8d6eb4cf627e26c2c33b891efe6bd201859c
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-02-14 18:17:30 +0000

    react imports

--- client/src/Scenes/Widgets/UserWidget.jsx
+++ client/src/Scenes/Widgets/UserWidget.jsx
@@ -1,12 +1,16 @@
 import { Box, Typography, Divider, useTheme } from '@mui/material';
 import WidgetWrapper from 'Components/WidgetWrapper';
 import FlexBetween from 'Components/FlexBetween';
+import { useNavigate } from 'react-router-dom';
 import UserImage from 'Components/UserImage';
+import { useEffect, useState} from 'react';
+import { useSelector } from 'react-redux';
 import {
   ManageAccountsOutlined,
   EditOutlined,
   LocationOnOutlined,
-  WorkOutlineOutlined
+  WorkOutlineOutlined,
+  FaxRounded
 } from '@mui/icons-material';
 
 const UserWidget = () => {
