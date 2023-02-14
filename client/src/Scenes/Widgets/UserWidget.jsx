ecf1dd379b364c10359761606db950e7939d296b
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-02-14 18:15:38 +0000

    create userWidget

---
+++ client/src/Scenes/Widgets/UserWidget.jsx
@@ -0,0 +1,20 @@
+import { Box, Typography, Divider, useTheme } from '@mui/material';
+import WidgetWrapper from 'Components/WidgetWrapper';
+import FlexBetween from 'Components/FlexBetween';
+import UserImage from 'Components/UserImage';
+import {
+  ManageAccountsOutlined,
+  EditOutlined,
+  LocationOnOutlined,
+  WorkOutlineOutlined
+} from '@mui/icons-material';
+
+const UserWidget = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UserWidget
