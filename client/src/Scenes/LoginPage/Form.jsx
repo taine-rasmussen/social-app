571d0bb1106ba1b836b1ea734070e66b4af1e5d2
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-02-14 14:07:42 +0000

    create schema for form

--- client/src/Scenes/LoginPage/Form.jsx
+++ client/src/Scenes/LoginPage/Form.jsx
@@ -1,4 +1,9 @@
+import EditOutlinedIcon from '@mui/icons-material/EditOutlinedIcon'
+import { useNavigate } from 'react-router-dom'
+import { useDisptach } from 'react-redux'
+import { Formik } from 'formik'
 import { useState } from 'react'
+import * as yup from 'yup'
 import { 
   Box, 
   Button, 
@@ -7,12 +12,25 @@ import {
   Typography, 
   useTheme 
 } from '@mui/material'
+import { setLogin } from 'state';
+import DropZone from 'react-dropzone';
+import FlexBetween from 'Components/FlexBetween'
+
+const registerSchema = yup.object().shape({
+  firstName: yup.string().required('required'),
+  lastName: yup.string().required('required'),
+  email: yup.string().email('invalid email').required('required'),
+  password: yup.string().required('required'),
+  location: yup.string().required('required'),
+  occupation: yup.string().required('required'),
+  picture: yup.string().required('required')
+})
 
 const Form = () => {
   return (
-    <div>
+    <Box>
       
-    </div>
+    </Box>
   )
 }
 
