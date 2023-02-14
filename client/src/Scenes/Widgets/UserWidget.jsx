0da154f0c0ee7f4b9862324890de3bea4a9935d3
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-02-14 18:23:13 +0000

    getUser api call

--- client/src/Scenes/Widgets/UserWidget.jsx
+++ client/src/Scenes/Widgets/UserWidget.jsx
@@ -4,7 +4,7 @@ import FlexBetween from 'Components/FlexBetween';
 import { useNavigate } from 'react-router-dom';
 import UserImage from 'Components/UserImage';
 import { useEffect, useState} from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   ManageAccountsOutlined,
   EditOutlined,
@@ -13,12 +13,34 @@ import {
   FaxRounded
 } from '@mui/icons-material';
 
-const UserWidget = () => {
+const UserWidget = ({ userId, picturePath }) => {
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
+  const token = useDispatch((state) => state.token);
+  
+  const { paletter } = useTheme();
+  const dark = palette.neutral.dark
+  const medium = palette.neutral.medium
+  const main = palette.neutral.main
+
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3001/users/${userId}`,
+    {
+      method: 'GET',
+      headers: { Authorization: `Bearer ${token}` },
+    });
+  };
+
+  useEffect(
+    () => {
+
+    })
+
   return (
     <div>
       
     </div>
   )
-}
+};
 
 export default UserWidget
