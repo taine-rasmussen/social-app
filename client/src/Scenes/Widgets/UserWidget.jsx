7d156a6f18e53d5cf4889af40c56533a623f6b85
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-02-14 22:49:40 +0000

    i am sooo sleepy

--- client/src/Scenes/Widgets/UserWidget.jsx
+++ client/src/Scenes/Widgets/UserWidget.jsx
@@ -97,7 +97,37 @@ const UserWidget = ({ userId, picturePath }) => {
           </Box>
         </Box>
 
+        <Box p='1rem 0'>
+          <FlexBetween mb='0.5rem'>
+            <Typography color={medium}>Who's viewed your profile</Typography>
+            <Typography color={main} fontWeight='500'>{viewedProfile}</Typography>
+          </FlexBetween>
+           <FlexBetween mb='0.5rem'>
+            <Typography color={medium}>Impressions of your post</Typography>
+            <Typography color={main} fontWeight='500'>{impressions}</Typography>
+          </FlexBetween>
+        </Box>
 
+        <Box p='1rem 0'>
+          <Typography
+            fontSize='1rem'
+            color={main}
+            fontWeight='500'
+            mb='0.5rem'
+          >
+            <FlexBetween gap='1rem' mb='0.5rem'>
+                <FlexBetween gap='1rem'>
+                  <img src='../assets/twitter.png' alt='twitter'/>
+                  <Box>
+                    <Typography color={main} fontWeight='500'>
+                      Twitter
+                    </Typography>
+                    <Typography color={medium}>Social Network</Typography>
+                  </Box>
+                </FlexBetween>
+            </FlexBetween>
+          </Typography>
+        </Box>
       </FlexBetween>
     </WidgetWrapper>
   )
