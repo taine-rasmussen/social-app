634dadf886009c5150351a5488c2ff0a3154d4ef
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-02-14 12:44:04 +0000

    start login page

--- client/src/Scenes/LoginPage/index.jsx
+++ client/src/Scenes/LoginPage/index.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import { Box, Typography, useTheme, useMediaQuery } from '@mui/material'
+import Form from './From'
 
 const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)")
+
+  const primaryLight = theme.palette.primary.light
+  const alt = theme.palette.neutral.alt;
+
   return (
-    <div>
-      LoginPage
-    </div>
+    <Box>
+      <Box 
+        width='100%'
+        backgroundColor={alt} 
+        p='1rem 6%'
+        textAlign='center'
+      >
+       <Typography
+          fontWeight='bold'
+          fontSize='32px'
+          color='primary'
+          sx={{
+            '&:hover': {
+              color: primaryLight,
+              cursor: 'pointer'
+            }
+          }}
+          >
+          Social App
+        </Typography>
+      </Box>
+      <Box
+        width={isNonMobileScreens ? '50%' : '93%'}
+        p='2rem'
+        m='2rem auto'
+        borderRadius='1.5rem'
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight='500' variant='h5' sx={{ mb: '1.5rem'}}>
+          Welcome to Social app, the Social media for you?
+        </Typography>
+
+      </Box>
+    </Box>
   )
 }
 
