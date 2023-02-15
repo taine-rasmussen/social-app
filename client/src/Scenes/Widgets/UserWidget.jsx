d6d43c5f8cf7a83435cd539c118e5ac3c41bc962
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-02-15 10:58:16 +0000

    fix 404

--- client/src/Scenes/Widgets/UserWidget.jsx
+++ client/src/Scenes/Widgets/UserWidget.jsx
@@ -26,7 +26,7 @@ const UserWidget = ({ userId, picturePath }) => {
  const getUser = async () => {
     const response = await fetch(`http://localhost:3001/users/${userId}`, {
       method: "GET",
-      headers: {Authorization: `Bearer ${token}`},
+      headers: { Authorization: `Bearer ${token}` },
     });
     const data = await response.json();
     setUser(data);
