0fa185a1d87190fcb9443d7597165bd36d8a0abe
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-02-15 10:36:27 +0000

    uhhhhh

--- client/src/Scenes/Widgets/UserWidget.jsx
+++ client/src/Scenes/Widgets/UserWidget.jsx
@@ -14,7 +14,7 @@ import {
 } from '@mui/icons-material';
 
 const UserWidget = ({ userId, picturePath }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState();
   const navigate = useNavigate();
   const token = useSelector((state) => state.token);
 
@@ -23,10 +23,10 @@ const UserWidget = ({ userId, picturePath }) => {
   const medium = palette.neutral.medium
   const main = palette.neutral.main
 
-  const getUser = async () => {
+ const getUser = async () => {
     const response = await fetch(`http://localhost:3001/users/${userId}`, {
       method: "GET",
-      headers: { Authorization: `Bearer ${token}` },
+      headers: {Authorization: `Bearer ${token}`},
     });
     const data = await response.json();
     setUser(data);
--- server/Controllers/Users.js
+++ server/Controllers/Users.js
@@ -2,11 +2,11 @@ import User from '../Models/User.js';
 
 // Read
 export const getUser = async (req, res) => {
-  try{
+  try {
     const { id } = req.params;
     const user = await User.findById(id);
-    res.status(200).json(200);
-  } catch(err) {
+    res.status(200).json(user);
+  } catch (err) {
     res.status(404).json({ message: err.message });
   }
 };
