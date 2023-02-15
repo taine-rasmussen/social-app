620e9ccb2cb04bbb67f6f0c9fa718ada86aafb81
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-02-15 14:12:34 +0000

    get all post data for feed

---
+++ client/src/Scenes/Widgets/PostsWidget.jsx
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { sePosts } from 'State';
+import PostWidget from './PostWidget';
+
+const PostsWidget = ({ useId, isProfile = false }) => {
+
+  const dispatch = useDispatch();
+  const posts = useSlector((state) => state.posts)
+  const token = useSlector((state) => state.token)
+
+  const getPosts = async () => {
+    const response = await fetch('https://localhost:3001/posts', {
+      method: 'GET',
+      headers: { Authorization: `Bearer ${token}` }
+    });
+    const data = await response.json();
+    dispatch(setPosts({ posts: data }));
+  };
+
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default PostsWidget
