2b60f16c9bccbffa1a2da446d190e26a9ade0cfa
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-02-18 16:51:27 +0000

    refactor mobilemenu out of nav index file

--- client/src/Scenes/NavBar/MobileNav.jsx
+++ client/src/Scenes/NavBar/MobileNav.jsx
@@ -2,9 +2,53 @@
 
 const MobileNav = () => {
   return (
-    <div>
+    <Box
+      sx={{
+        backgroundColor: background,
+        maxWidth: '175px',
+        minWidth: '75px',
+        padding: '0.75rem 0',
+        borderRadius: '0.5rem',
+        zIndex: '999999'
+      }}
+    >
+      <Box
+        display='flex'
+        justifyContent='flex-end'
+      >
+        <IconButton
+          onClick={() => { setIsMobileMenuToggled(!isMobileMenuToggled) }}
+        >
+          <Close />
+        </IconButton>
+      </Box>
+      <Divider />
+      <FlexBetween
+        display='flex'
+        flexDirection='row'
+        justifyContent='center'
+        alignItems='center'
+        gap='0.5rem'
+        padding='0.5rem'
+      >
 
-    </div>
+        {theme.palette.mode === 'dark' ? (
+          <DarkMode sx={{ fontSize: '25px' }} onClick={() => dispatch(setMode())} />
+        ) : (
+          <LightMode sx={{ color: dark, fontSize: '25px' }} onClick={() => dispatch(setMode())} />
+        )}
+        <Message sx={{ fontSize: '25px' }} />
+        <Notifications sx={{ fontSize: '25px' }} />
+        <Help sx={{ fontSize: '25px' }} />
+        <LogoutIcon
+          sx={{ fontSize: '25px' }}
+          onClick={() => {
+            dispatch(setLogout())
+            navigate('/')
+          }}
+        />
+      </FlexBetween>
+    </Box>
   )
 }
 
--- client/src/Scenes/NavBar/index.jsx
+++ client/src/Scenes/NavBar/index.jsx
@@ -168,53 +168,7 @@ const NavBar = () => {
       )}
       {/* Mobile Nav */}
       {!isNonMobileScreens && isMobileMenuToggled && (
-        <Box
-          sx={{
-            backgroundColor: background,
-            maxWidth: '175px',
-            minWidth: '75px',
-            padding: '0.75rem 0',
-            borderRadius: '0.5rem',
-            zIndex: '999999'
-          }}
-        >
-          <Box
-            display='flex'
-            justifyContent='flex-end'
-          >
-            <IconButton
-              onClick={() => { setIsMobileMenuToggled(!isMobileMenuToggled) }}
-            >
-              <Close />
-            </IconButton>
-          </Box>
-          <Divider />
-          <FlexBetween
-            display='flex'
-            flexDirection='row'
-            justifyContent='center'
-            alignItems='center'
-            gap='0.5rem'
-            padding='0.5rem'
-          >
-
-            {theme.palette.mode === 'dark' ? (
-              <DarkMode sx={{ fontSize: '25px' }} onClick={() => dispatch(setMode())} />
-            ) : (
-              <LightMode sx={{ color: dark, fontSize: '25px' }} onClick={() => dispatch(setMode())} />
-            )}
-            <Message sx={{ fontSize: '25px' }} />
-            <Notifications sx={{ fontSize: '25px' }} />
-            <Help sx={{ fontSize: '25px' }} />
-            <LogoutIcon
-              sx={{ fontSize: '25px' }}
-              onClick={() => {
-                dispatch(setLogout())
-                navigate('/')
-              }}
-            />
-          </FlexBetween>
-        </Box>
+        <MobileNav />
       )}
     </FlexBetween>
   )
